10f60a083ea128c6ef8cf6201b4c89787048e8b1
luoe <luo.e@yahoo.com>
2020-05-30 00:19:44 +0800

    resources

--- src/main/java/com/example/demo/controller/CommDisasterController.java
+++ src/main/java/com/example/demo/controller/CommDisasterController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/testCommD")
+@RequestMapping("/CommDisaster")
 public class CommDisasterController {
     @Autowired(required = false)
     private  CommDisasterService commDisasterService;
